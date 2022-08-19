08ca8e150c515138f042ce5c140ede708287f9e2
arthur miranda <miranda_chaves@hotmail.com>
2022-08-19 09:31:41 -0300

    deploy

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
-// import Login from './Login';
+import './components/assets/fonts/stylesheet.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
